82ec643eb49d6916405c13995cef0e54ea4d8aa6
HwanLee358 <clse0286@naver.com>
2024-06-26 08:58:53 +0900

    init

---
+++ bankend/routes/AdminOrder.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router()
+const query = require("../mysql")
+
+router.get("/", (req, res) => {
+    
+})
+
+
+
+module.exports = router;
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "/api" :{
+        target: 'http://localhost:3000',
+        ws: false,
+        changeOrigin: true,
+        pathRewrite: {'^/api': ''}
+      }
+    }
+  }
 })
