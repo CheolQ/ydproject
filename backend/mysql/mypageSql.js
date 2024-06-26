6c3fdab648278741141ba5ffee74deb967473d03
Jinhyoung Kim <jinhyoung0213@gmail.com>
2024-06-26 14:48:37 +0900

    20240626

---
+++ backend/mysql/mypageSql.js
@@ -0,0 +1,20 @@
+module.exports = {
+    userInfo: `select 
+                    name, 
+                    user_id, 
+                    email, 
+                    addr, 
+                    detail_addr, 
+                    point, 
+                    (
+                        select sum(pay_price) 
+                        from orders
+                        where user_no = (
+							select user_no
+                            from user
+                            where user_id = ?
+                        )
+                    ) as total_order_price
+                from user
+                where user_id = ?`
+}
