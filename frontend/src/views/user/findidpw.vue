43ed759703cdd8ad076ba70c0f759624417bbb18
Jinhyoung Kim <jinhyoung0213@gmail.com>
2024-06-29 16:06:36 +0900

    css 충돌수정

--- frontend/src/views/user/findidpw.vue
+++ frontend/src/views/user/findidpw.vue
@@ -1,48 +1,52 @@
 <template>
+
     <body>
-    <div class="login-wrapper">
-        <h2>아이디 찾기</h2>
-        <form method="post" action="서버의url" id="login-form">
-            <input type="text" name="userName" placeholder="Name" v-model="name">
-            <input type="text" name="userPassword" placeholder="TEL" v-model="tel">
-            <label for="remember-check">
-                <input type="checkbox" id="remember-check">아이디 저장하기
-            </label>
-            <input type="submit" value="Login">
-        </form>
-    </div>
-</body>
-    </template>
-    <script>
-    export default {
-    data(){return {}; },
-    created () {},
-    methods : {},
-    }
-    </script>
-    <style>
-*{
+        <div class="login-wrapper">
+            <h2>아이디 찾기</h2>
+            <form method="post" action="서버의url" id="login-form">
+                <input type="text" name="userName" placeholder="Name" v-model="name">
+                <input type="text" name="userPassword" placeholder="TEL" v-model="tel">
+                <label for="remember-check">
+                    <input type="checkbox" id="remember-check">아이디 저장하기
+                </label>
+                <input type="submit" value="Login">
+            </form>
+        </div>
+    </body>
+</template>
+<script>
+export default {
+    data() { return {}; },
+    created() { },
+    methods: {},
+}
+</script>
+<style scoped>
+* {
     padding: 0;
     margin: 0;
     border: none;
 }
-body{
+
+body {
     font-size: 14px;
     font-family: 'Roboto', sans-serif;
 }
-.login-wrapper{
+
+.login-wrapper {
     width: 400px;
     height: 350px;
     padding: 40px;
     box-sizing: border-box;
 }
 
-.login-wrapper > h2{
+.login-wrapper>h2 {
     font-size: 24px;
     color: #6A24FE;
     margin-bottom: 20px;
 }
-#login-form > input{
+
+#login-form>input {
     width: 100%;
     height: 48px;
     padding: 0 10px;
@@ -51,10 +55,12 @@ body{
     border-radius: 6px;
     background-color: #F8F8F8;
 }
-#login-form > input::placeholder{
+
+#login-form>input::placeholder {
     color: #D2D2D2;
 }
-#login-form > input[type="submit"]{
+
+#login-form>input[type="submit"] {
     color: #fff;
     font-size: 16px;
     background-color: #6A24FE;
--- frontend/src/views/user/joinForm.vue
+++ frontend/src/views/user/joinForm.vue
@@ -11,7 +11,7 @@
         </div>
         <div>
             <label for="password2">비밀번호확인</label>
-            <input type="password" id="password2" v-model="form.user_pw2"/>
+            <input type="password" id="password2" v-model="form.user_pw2" />
         </div>
         <div>
             <label for="name">이름</label>
@@ -23,7 +23,7 @@
         </div>
         <div>
             <label for="phone">전화번호</label>
-            <input type="tel" id="tel" v-model="form.tel" placeholder="ex) 010-1111-1111"/>
+            <input type="tel" id="tel" v-model="form.tel" placeholder="ex) 010-1111-1111" />
         </div>
         <div>
             <label for="postcode">우편번호</label>
@@ -36,7 +36,7 @@
         </div>
         <div>
             <label for="detailAddress">상세주소</label>
-            <input type="text" id="detailAddress" v-model="form.detail_address" placeholder="상세주소"/>
+            <input type="text" id="detailAddress" v-model="form.detail_address" placeholder="상세주소" />
         </div>
         <button class="btn btn-primary" @click="joinUser()">회원가입</button>
     </div>
@@ -101,48 +101,48 @@ export default {
             alert("회원가입에 성공하셨습니다");
         }
     },
-        
+
 
     showApi() {
-      new window.daum.Postcode({
-        oncomplete: (data) => {
-            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+        new window.daum.Postcode({
+            oncomplete: (data) => {
+                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 
-            // 도로명 주소의 노출 규칙에 따라 주소를 조합한다.
-            // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-            let fullRoadAddr = data.roadAddress; // 도로명 주소 변수
-            let extraRoadAddr = ''; // 도로명 조합형 주소 변수
+                // 도로명 주소의 노출 규칙에 따라 주소를 조합한다.
+                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+                let fullRoadAddr = data.roadAddress; // 도로명 주소 변수
+                let extraRoadAddr = ''; // 도로명 조합형 주소 변수
 
-            // 법정동명이 있을 경우 추가한다. (법정리는 제외)
-            // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-            if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
-                extraRoadAddr += data.bname;
-            }
-            // 건물명이 있고, 공동주택일 경우 추가한다.
-            if(data.buildingName !== '' && data.apartment === 'Y'){
-              extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-            }
-            // 도로명, 지번 조합형 주소가 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-            if(extraRoadAddr !== ''){
-                extraRoadAddr = ' (' + extraRoadAddr + ')';
-            }
-            // 도로명, 지번 주소의 유무에 따라 해당 조합형 주소를 추가한다.
-            if(fullRoadAddr !== ''){
-                fullRoadAddr += extraRoadAddr;
-            }
+                // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+                    extraRoadAddr += data.bname;
+                }
+                // 건물명이 있고, 공동주택일 경우 추가한다.
+                if (data.buildingName !== '' && data.apartment === 'Y') {
+                    extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+                // 도로명, 지번 조합형 주소가 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                if (extraRoadAddr !== '') {
+                    extraRoadAddr = ' (' + extraRoadAddr + ')';
+                }
+                // 도로명, 지번 주소의 유무에 따라 해당 조합형 주소를 추가한다.
+                if (fullRoadAddr !== '') {
+                    fullRoadAddr += extraRoadAddr;
+                }
 
-            // 우편번호와 주소 정보를 해당 필드에 넣는다.
-            this.form.postcode = data.zonecode; //5자리 새우편번호 사용
-            this.form.address = fullRoadAddr;
-        }
-    }).open()
+                // 우편번호와 주소 정보를 해당 필드에 넣는다.
+                this.form.postcode = data.zonecode; //5자리 새우편번호 사용
+                this.form.address = fullRoadAddr;
+            }
+        }).open()
     }
-  }
+}
 
 
 </script>
 
-<style>
+<style scoped>
 .aside-tit {
     padding: 65px 0 30px;
     font-size: 24px;
