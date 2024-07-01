0e0cdcaa07dd479a09e6334fb60fdceb2033c803
juhihun <wngmlgus7@naver.com>
2024-07-01 20:07:45 +0900

    20240701h

--- backend/mysql/prodSql.js
+++ backend/mysql/prodSql.js
@@ -15,13 +15,8 @@ module.exports = {
             where prod_no = ? and board_no = ?`,
 
     qnaInsert: `insert into qna set ?`,
+    
     qnaUpdate: 'UPDATE qna SET ? WHERE board_no = ?',
 
     selectuserno: `select user_no from user where user_id = ?`,
-    prodCategory : `select c.category_code, c.category_name, p.prod_name, p.prod_no
-                    from prod as p
-                        left join category as c
-                        on p.category_code = c.category_code
-                    where category_code = ?`
-
 };
