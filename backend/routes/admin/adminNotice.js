0a981aab0c05e43fab3b0468a65c5ffd3de52e56
HwanLee358 <clse0286@naver.com>
2024-07-04 19:06:51 +0900

    init

--- backend/routes/admin/adminNotice.js
+++ backend/routes/admin/adminNotice.js
@@ -8,12 +8,22 @@ router.get("/", async (req, res) => {
     let page = Number(req.query.page);
     let pageUnit = Number(req.query.pageUnit);
 
+    let name = '%'+req.query.name+'%';
+    let date1 = req.query.date1;
+    let date2 = req.query.date2;
+    let category = '%'+req.query.category+'%';
+
+    if(!date2) {
+        date2 = await query("AdminExpdate");
+        date2 = date2[0].date;
+    }
+
     if(!page){ page = 1; }
     if(!pageUnit){ pageUnit = 10;}
     let offset = (page-1) * pageUnit;
 
-    let list = await query("AdminNoticeList",[offset, pageUnit]);
-    let count = await query("AdminNoticCount")
+    let list = await query("AdminNoticeList",[name, date1, date2, category, offset, pageUnit]);
+    let count = await query("AdminNoticCount", [name, date1, date2, category])
     res.send({list, count})
 })
 
@@ -56,7 +66,7 @@ router.post("/",upload.array("files"), async (req, res) => {
     console.log(data)
     console.log(req.files)
     console.log(req.files.length);
-    await query("AdminNoticeInsert", [req.session.user_id ,data.title, data.content, req.session.user_no])
+    await query("AdminNoticeInsert", [data.category_code, req.session.user_id ,data.title, data.content, req.session.user_no])
     .then(result=>console.log(result))
     .catch(err => console.log(err))
     let table = await query("AdminNoticeLastNo");
